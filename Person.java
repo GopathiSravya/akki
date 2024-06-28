687c5be9610485fdf2ac7ca81b1ddd45b234e610
sravya <sraavya01@gmail.com>
2024-06-28 11:13:37 +0530

    name is sravya,id is 555

---
+++ Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    public static void main(String[] args) {
+        String name="sravya";
+        int id=555;
+        System.out.println(name);
+        System.out.println(id);
+    }
+}
